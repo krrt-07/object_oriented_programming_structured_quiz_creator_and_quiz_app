2776991394711cc8ba8e366cd88419a5e914d1f1
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-05-27 22:43:36 +0800

    run function to start the quiz creator.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,8 @@
 # Import the parent class from main menu (base) file, and create a child class for quiz creator.
 from quiz_main_menu_base import quiz_base
+#for testing purposes.
+import sys
+sys.dont_write_bytecode = True
 
 class quiz_creator(quiz_base):
     def run(self):
@@ -13,29 +16,34 @@ class quiz_creator(quiz_base):
                 print("Exiting quiz creator.")
                 break
 
-        # add 1 to the question number if the user type a question.
-        question_number += 1
+            # add 1 to the question number if the user type a question.
+            question_number += 1
 
-        # ask the user to input the question.
-        choices = {
-            "A": input("Enter choice A: "),
-            "B": input("Enter choice B: "),
-            "C": input("Enter choice C: "),
-            "D": input("Enter choice D: "),
-        }
+            # ask the user to input the question.
+            choices = {
+                "A": input("Enter choice A: "),
+                "B": input("Enter choice B: "),
+                "C": input("Enter choice C: "),
+                "D": input("Enter choice D: "),
+            }
 
-        # input the correct answer.
-        correct = input("Enter the correct answer (A, B, C, D): ").upper()
+            # input the correct answer.
+            correct = input("Enter the correct answer (A, B, C, D): ").upper()
 
-        # save the question and answers to a text file.
-        data = {
-            "number": question_number,
-            "question": question,
-            "A": choices["A"],
-            "B": choices["B"],
-            "C": choices["C"],
-            "D": choices["D"],
-            "correct": correct
-        }
+            # save the question and answers to a text file.
+            question_data = {
+                "number": question_number,
+                "question": question,
+                "A": choices["A"],
+                "B": choices["B"],
+                "C": choices["C"],
+                "D": choices["D"],
+                "correct": correct
+            }
 
-        self.save_question(data)
+            self.save_question(question_data)
+
+# run function to start the quiz creator.
+if __name__ == "__main__":
+    creator = quiz_creator()
+    creator.run()
